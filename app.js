49b026fd05f30c546a615fe5e9bb7374c16e770c
ElianFernandez2021 <bastianfernandez907@gmail.com>
2022-10-26 18:56:38 -0300

    Primer commit

---
+++ app.js
@@ -0,0 +1,16 @@
+/*Variables*/
+const express = require("express");
+const app = express();
+const path = require("path")
+
+/*Middlewares*/
+app.use(express.static(path.join(__dirname, 'public')));
+
+/*Routes*/
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "/src/views/index.html"))
+});
+
+app.listen(3000, ()=>
+    console.log("Servidor corriendo en: http://localhost:3000/")
+);
