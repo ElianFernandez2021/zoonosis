6dbc463dba07e244fc01d1d4a0d310416e53f20e
ElianFernandez2021 <bastianfernandez907@gmail.com>
2022-11-02 16:54:09 -0300

    Agregando antes y después

--- app.js
+++ app.js
@@ -14,6 +14,10 @@ app.get("/", (req, res) => {
 app.get("/somos", (req, res) => {
     res.sendFile(path.join(__dirname, "/src/views/quienesSomos.html"))
 });
+app.get("/antesDespues", (req, res) => {
+    res.sendFile(path.join(__dirname, "/src/views/antesDespues.html"))
+});
+
 
 app.listen(3000, ()=>
     console.log("Servidor corriendo en: http://localhost:3000/")
